b07c3c46b17a8b68788512010864d2e5dad92875
Matius Rimbe <matiusrimbe.ptik@gmail.com>
2025-12-02 22:59:26 +0800

    create feature api to get all posts

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function() {
+    Route::prefix('posts')->group(function() {
+        Route::get('/', [PostController::class, 'index']);
+        Route::post('/', [PostController::class, 'store']);
+        Route::get('{id}', [PostController::class, 'show']);
+        Route::put('{id}', [PostController::class, 'update']);
+        Route::delete('{id}', [PostController::class, 'destroy']);
+    });
+});
