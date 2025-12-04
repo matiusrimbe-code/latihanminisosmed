e0278b87d093b2b2f995e63755397d82e8bc41ee
Matius Rimbe <matiusrimbe.ptik@gmail.com>
2025-12-04 20:44:56 +0800

    feature create a new post, get detail post, update post and delete post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -13,6 +14,77 @@ class PostController extends Controller
         return response()->json([
             'success' => true,
             'data' => $posts
+        ]);
+    }
+
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'content' => 'required|string|max:255',
+            'image_url' => 'nullable'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $post = Post::create([
+            'user_id' => $request->user_id,
+            'content' => $request->content,
+            'image_url' => $request->image_url
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post berhasil dibuat',
+            'data' => $post
+        ], 201);
+    }
+
+    public function show($id) {
+        $post = Post::find($id);
+
+        return response()->json([
+            'success' => true,
+            'data' => $post
+        ]);
+    }
+
+    public function update($id, Request $request) {
+        $validator = Validator::make($request->all(), [
+            'content' => 'required|string|max:255',
+            'image_url' => 'nullable'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $post = Post::find($id);
+        $post->content = $request->content;
+        $post->image_url = $request->image_url;
+
+        $post->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post berhasil di update',
+            'data' => $post
+        ]);
+    }
+
+    public function destroy(int $id) {
+        Post::destroy($id);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data berhasil dihapus'
         ]);
     }
 }
